8eeed3411f98652f62ad6104f60387a73168fa92
Vasile Midrigan <111rigan111@gmail.com>
2023-09-25 23:12:09 +0100

    implement frontend component ProductsMenu

--- src/app/components/productsMenu/ProductsMenu.jsx
+++ src/app/components/productsMenu/ProductsMenu.jsx
@@ -1,5 +1,25 @@
+import { productsListing } from "@/data/products-listing";
+
 export default function ProductsMenu() {
   return (
-    <div className="ProductsMenu"></div>
-  )
+    <div className="ProductsMenu">
+      <div className="ProductsMenu__categories">
+        {productsListing.map((category) => (
+          <div key={category.id}>
+            {category.id}
+          </div>
+        ))}
+      </div>
+      <div className="ProductsMenu__subcategories">
+        <div className="ProductsMenu__subcategory-wrapper">
+          <span className="ProductsMenu__subcategory">Subcategory-A</span>
+          <ul className="ProductsMenu__items">
+            <li>item a</li>
+            <li>item b</li>
+            <li>item c</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/app/data/products-listing.jsx
@@ -0,0 +1,59 @@
+export const productsListing = [
+  {
+    id: "Smartphones & Tablets",
+    subcategories: [
+      {
+        id: "Smartphones",
+        items: ["Iphone", "Samsung", "Sony", "Xiaomi", "Huawei"],
+      },
+      {
+        id: "Tablets",
+        items: [],
+      },
+    ],
+  },
+  {
+    id: "Computers & Displays",
+    subcategories: [
+      {
+        id: "Computers",
+        items: [],
+      },
+      {
+        id: "Displays",
+        items: [],
+      },
+    ],
+  },
+  {
+    id: "Photo & Video",
+    subcategories: [
+      {
+        id: "Photo",
+        items: [],
+      },
+      {
+        id: "Video",
+        items: [],
+      },
+    ],
+  },
+  {
+    id: "Video Games",
+    subcategories: [
+      {
+        id: "Consoles",
+        items: [],
+      } 
+    ],
+  },
+  {
+    id: "Accessories",
+    subcategories: [
+      {
+        id: "Smart Watches",
+        items: [],
+      }
+    ],
+  },
+];
