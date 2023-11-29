0e5f722032c85b07c7ff064a8647f7a535eedbd2
Vasile Midrigan <111rigan111@gmail.com>
2023-11-29 09:31:17 +0000

    Style CategoryIllustration component

--- src/components/category/categoryIllustration/CategoryIllustration.jsx
+++ src/components/category/categoryIllustration/CategoryIllustration.jsx
@@ -8,7 +8,9 @@ export default function CategoryIllustration({ categoryIllustrationImage }) {
   return (
     <div className="CategoryIllustration">
       <h2 className="CategoryIllustration__header">Smartphones</h2>
-      <Image src={categoryIllustrationImage.url} alt="two smartphones" fill />
+      <div className="CategoryIllustration__img-wrapper">
+        <Image src={categoryIllustrationImage.url} alt="two smartphones" fill />
+      </div>
     </div>
   );
 }
